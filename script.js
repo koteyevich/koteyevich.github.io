14187c7997c2a3451464ec73353634b8fb19b2bc
koteyevich <koteyevich@ukr.net>
2024-07-07 16:31:44 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function playSpringAnimation(element) {
-    element.classList.add('epic-ease-spring');
-    // After some delay, remove the class to allow the animation to be played again on subsequent clicks
-    setTimeout(function() {
-        element.classList.remove('epic-ease-spring');
-    }, 1500); // The same duration as your animation
-}
